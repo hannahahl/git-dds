5f854b5ac1eed6ca8c1fc53514136822622e6439
Max Less <maxless91@gmail.com>
2013-05-13 10:02:28 +0200

    Added script.js
    YO YO YO

---
+++ script.js
@@ -0,0 +1,11 @@
+    window.onload = function() {
+      var node = document.getElementById('blinking-text');
+      setInterval(function() {
+        visibility = node.style.visibility;
+        if (visibility == "") {
+          node.style.visibility = "hidden";
+        } else {
+          node.style.visibility = "";
+        }
+      }, 800);
+    }
